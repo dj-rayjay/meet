5051f8c1725640525f9dfbba24bc47ac8e7104dc
odotom <thomas.odorfer@posteo.com>
2014-12-24 07:22:14 +0100

    created web storm env and configuration to interwork with meet.iparlo.net

--- config.js
+++ config.js
@@ -1,34 +1,34 @@
 var config = {
     hosts: {
-        domain: 'jitsi-meet.example.com',
+        domain: 'meet.iparlo.net',
         //anonymousdomain: 'guest.example.com',
-        muc: 'conference.jitsi-meet.example.com', // FIXME: use XEP-0030
-        bridge: 'jitsi-videobridge.jitsi-meet.example.com', // FIXME: use XEP-0030
-        //call_control: 'callcontrol.jitsi-meet.example.com',
-        //focus: 'focus.jitsi-meet.example.com' - defaults to 'focus.jitsi-meet.example.com'
+        muc: 'conference.meet.iparlo.net', // FIXME: use XEP-0030
+        bridge: 'jitsi-videobridge.meet.iparlo.net', // FIXME: use XEP-0030
+        call_control: 'callcontrol.meet.iparlo.net' //,
+        //focus: 'focus.meet.iparlo.net' - defaults to 'focus.meet.iparlo.net'
     },
 //  getroomnode: function (path) { return 'someprefixpossiblybasedonpath'; },
 //  useStunTurn: true, // use XEP-0215 to fetch STUN and TURN server
 //  useIPv6: true, // ipv6 support. use at your own risk
     useNicks: false,
-    bosh: '//jitsi-meet.example.com/http-bind', // FIXME: use xep-0156 for that
-    clientNode: 'http://jitsi.org/jitsimeet', // The name of client node advertised in XEP-0115 'c' stanza
-    //focusUserJid: 'focus@auth.jitsi-meet.example.com', // The real JID of focus participant - can be overridden here
+    bosh: '//meet.iparlo.net/http-bind', // FIXME: use xep-0156 for that
+    clientNode: 'http://meet.iparlo.net/', // The name of client node advertised in XEP-0115 'c' stanza
+    //focusUserJid: 'focus@auth.meet.iparlo.net', // The real JID of focus participant - can be overridden here
     //defaultSipNumber: '', // Default SIP number
     desktopSharing: 'ext', // Desktop sharing method. Can be set to 'ext', 'webrtc' or false to disable.
-    chromeExtensionId: 'diibjkoicjeejcmhdnailmkgecihlobk', // Id of desktop streamer Chrome extension
+    chromeExtensionId: 'fbalddklnheneegehijmjanjbjimckgi', // Id of desktop streamer Chrome extension
     desktopSharingSources: ['screen', 'window'],
-    minChromeExtVersion: '0.1', // Required version of Chrome extension
+    minChromeExtVersion: '0.0.0.1', // Required version of Chrome extension
     enableRtpStats: true, // Enables RTP stats processing
     openSctp: true, // Toggle to enable/disable SCTP channels
     channelLastN: -1, // The default value of the channel attribute last-n.
     adaptiveLastN: false,
     adaptiveSimulcast: false,
-    useRtcpMux: true,
+    useRtcpMux: false,
     useBundle: true,
     enableRecording: false,
-    enableWelcomePage: true,
-    enableSimulcast: false,
+    enableWelcomePage: false,
+    enableSimulcast: true,
     enableFirefoxSupport: false, //firefox support is still experimental, only one-to-one conferences with chrome focus
     // will work when simulcast, bundle, mux, lastN and SCTP are disabled.
     logStats: false // Enable logging of PeerConnection stats via the focus
